31199285a2f5eb74c673b40ad90ece38481cd543
Sergiusz <sergiusz.sanetra@gmail.com>
2023-04-15 21:45:17 +0200

    update

--- js/nowyprzepis.js
+++ js/nowyprzepis.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const textarea = document.querySelector('.textarea-krok-przygotowania');
 
     textarea.addEventListener('input', () => {
-        textarea.style.height = 'auto';
-        textarea.style.height = textarea.scrollHeight + 'px';
+        // textarea.style.height = 'auto';
+        // textarea.style.height = textarea.scrollHeight + 'px';
     });
 });
